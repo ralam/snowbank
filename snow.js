b4de4082a33dbf6041a4e620166c3f978aebbcf6
ralam <rehan.alam@gmail.com>
2015-10-07 20:50:49 -0700

    rework how snowbanks are created

--- snow.js
+++ snow.js
@@ -9,23 +9,24 @@
     this.snowflakes = [];
     this.snowbank = [];
     this.movingSnowflakes = [];
+    this.boundary = Array.apply(null, {length: y}).map(Number.call, Number)
     for(var i = 0; i < this.NUM_SNOWFLAKES; i++) {
       newSnowflake = Display.Snowflake.addRandomSnowflake(x, y);
       this.snowflakes.push(newSnowflake);
     }
   };
 
-  Snow.prototype.NUM_SNOWFLAKES = 250;
+  Snow.prototype.NUM_SNOWFLAKES = 500;
 
   Snow.prototype.moveSnowflakes = function(ctx) {
     this.movingSnowflakes = [];
 
     this.snowflakes.forEach(function(snowflake) {
-      var futurePos = snowflake.futurePos();
-      this.checkCollisions(snowflake, futurePos);
       if (snowflake.moving) {
         snowflake.move();
+        this.checkCollisions(snowflake);
       }
+      this.checkCollisions(snowflake, snowflake.pos);
     }.bind(this));
     this.snowbank = []
     this.snowflakes.forEach(function(snowflake){
@@ -44,23 +45,23 @@
 
   };
 
-  Snow.prototype.isCollidedWith = function(pos, otherFlake) {
+  Snow.prototype.isCollidedWith = function(snowflake, otherFlake) {
     var that = this;
-    var xDist = Math.abs(pos[0] - otherFlake[0]);
-    var yDist = Math.abs(pos[1] - otherFlake[1]);
+    var xDist = Math.abs(snowflake.pos[0] - otherFlake[0]);
+    var yDist = Math.abs(snowflake.pos[1] - otherFlake[1]);
     var distBetween = Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2));
-    if (distBetween < (4) && pos[0] != otherFlake[0] && pos[1] != otherFlake[1]){
+    if (distBetween < (4) && snowflake.pos[0] != otherFlake[0] && snowflake.pos[1] != otherFlake[1]){
       return true;
     } else {
       return false;
     }
   };
 
-  Snow.prototype.checkCollisions = function(snowflake, futurePos){
+  Snow.prototype.checkCollisions = function(snowflake){
     var that = this;
 
     this.snowbank.forEach(function(stoppedSnowflake){
-      if (this.isCollidedWith(futurePos, stoppedSnowflake)) {
+      if (this.isCollidedWith(snowflake, stoppedSnowflake)) {
         snowflake.stick();
       };
     }.bind(this));
